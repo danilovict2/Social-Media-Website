07459949f4e0eea6fc2e555ec26d893c53f291de
Danilo <danilosindric@gmail.com>
2024-02-26 20:59:33 +0100

    Add vue

--- assets/app.js
+++ assets/app.js
@@ -1,2 +1,6 @@
+import { registerVueControllerComponents } from '@symfony/ux-vue';
+import './bootstrap.js';
 import './styles/app.css';
 
+
+registerVueControllerComponents(require.context('./vue/controllers', true, /\.vue$/));
